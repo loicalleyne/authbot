badcc2fac5b2b50f595425a40956c59429fa9a85
loicalleyne <loicalleyne@gmail.com>
2023-05-12 10:57:38 -0400

    removed logging, err to ErrorChan

--- authbot.go
+++ authbot.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
 	"time"
@@ -22,9 +21,11 @@ import (
 
 type Secret struct {
 	Token         atomic.String
+	CancelFunc    context.CancelFunc
+	ErrorChan     chan error
 	token         []byte
 	url           string
-	secretID      string
+	SecretID      string
 	secretVersion string
 	tokenType     string
 	tokenField    string
@@ -35,6 +36,9 @@ var (
 	secretCache, _ = secretcache.New()
 )
 
+// Load gets authbot to retrieve the stored secret from GCP or AWS Secret Manager
+// and stores the secret in keyring or launches the authorization renewal goroutines
+// which will retrieve tokens and store them in keyring
 func Load() error {
 	secretLocation := os.Getenv("SECRET_STORE")
 	if secretLocation == "" {
@@ -64,8 +68,8 @@ func Load() error {
 		for i := 1; i <= int(secretCount); i++ {
 			var err error
 			s := new(Secret)
-			s.secretID = os.Getenv("SECRET_ID_" + cast.ToString(i))
-			if s.secretID == "" {
+			s.SecretID = os.Getenv("SECRET_ID_" + cast.ToString(i))
+			if s.SecretID == "" {
 				return fmt.Errorf("authcreds: missing secret_id")
 			}
 			s.secretVersion = os.Getenv("SECRET_VERSION_" + cast.ToString(i))
@@ -90,9 +94,9 @@ func Load() error {
 				s.tokenField = os.Getenv("TOKEN_FIELD_" + cast.ToString(i))
 			}
 
-			s.token, err = fetchGCPSecret(projectID, s.secretID, s.secretVersion)
+			s.token, err = fetchGCPSecret(projectID, s.SecretID, s.secretVersion)
 			if err != nil {
-				return fmt.Errorf("authcreds: error retrieving %v/version/%v: %v", s.secretID, s.secretVersion, err)
+				return fmt.Errorf("authcreds: error retrieving %v/version/%v: %v", s.SecretID, s.secretVersion, err)
 			}
 			// add secret to keyring
 			Keyring = append(Keyring, *s)
@@ -100,8 +104,12 @@ func Load() error {
 		// launch goroutine to fetch bearer tokens or store API key
 		for i := 0; i < len(Keyring); i++ {
 			if Keyring[i].tokenType == "Bearer" {
-				go authBearer(Keyring, i, "")
-				go authBearer(Keyring, i, "120s")
+				authCtx := context.Background()
+				authCtx, authCancel := context.WithCancel(authCtx)
+				Keyring[i].CancelFunc = authCancel
+				errors := make(chan error, 0)
+				Keyring[i].ErrorChan = errors
+				go authBearer(authCtx, &Keyring, i)
 			} else {
 				Keyring[i].Token.Store(string(Keyring[i].token))
 			}
@@ -110,8 +118,8 @@ func Load() error {
 		for i := 1; i <= int(secretCount); i++ {
 			var err error
 			s := new(Secret)
-			s.secretID = os.Getenv("SECRET_ID_" + cast.ToString(i))
-			if s.secretID == "" {
+			s.SecretID = os.Getenv("SECRET_ID_" + cast.ToString(i))
+			if s.SecretID == "" {
 				return fmt.Errorf("authcreds: missing secret_id")
 			}
 
@@ -132,9 +140,9 @@ func Load() error {
 					s.tokenField = "access_token"
 				}
 			}
-			token, err := fetchAWSSecret(s.secretID)
+			token, err := fetchAWSSecret(s.SecretID)
 			if err != nil {
-				return fmt.Errorf("authcreds: error retrieving AWS secret %v: %v", s.secretID, err)
+				return fmt.Errorf("authcreds: error retrieving AWS secret %v: %v", s.SecretID, err)
 			}
 			s.token = []byte(token)
 
@@ -144,8 +152,12 @@ func Load() error {
 		// launch goroutine to fetch bearer tokens or store API key
 		for i := 0; i < len(Keyring); i++ {
 			if Keyring[i].tokenType == "BEARER" {
-				go authBearer(Keyring, i, "")
-				go authBearer(Keyring, i, "120s")
+				authCtx := context.Background()
+				authCtx, authCancel := context.WithCancel(authCtx)
+				Keyring[i].CancelFunc = authCancel
+				errors := make(chan error, 0)
+				Keyring[i].ErrorChan = errors
+				go authBearer(authCtx, &Keyring, i)
 			} else {
 				Keyring[i].Token.Store(string(Keyring[i].token))
 			}
@@ -181,42 +193,47 @@ func fetchGCPSecret(projectID, secretID, secretVersion string) ([]byte, error) {
 	return result.Payload.Data, nil
 }
 
-func authBearer(Keyring []Secret, index int, overlap string) {
-	s := Keyring[index]
-	payload := strings.NewReader(string(s.token))
-	req, err := retryablehttp.NewRequest("POST", s.url, payload)
+// authBearer is launched as a goroutine
+func authBearer(ctx context.Context, Keyring *[]Secret, index int) {
+	s := *Keyring
+
+	payload := strings.NewReader(string(s[index].token))
+	req, err := retryablehttp.NewRequest("POST", s[index].url, payload)
 	if err != nil {
-		log.Println(err)
+		s[index].ErrorChan <- err
 	}
 	retryClient := retryablehttp.NewClient()
-	retryClient.RetryMax = 2
-	retryClient.RetryWaitMin = 10000000
+	retryClient.RetryMax = 30
+	retryClient.RetryWaitMin = 2 * time.Second
 	retryClient.Logger = nil
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Cache-Control", "no-cache")
 	var expiry int64
-	time.Sleep(cast.ToDuration(overlap))
-	for {
-		res, err := retryClient.Do(req)
+	for ctx.Err() == nil {
+		resp, err := retryClient.Do(req)
 		if err != nil {
-			log.Println(err)
+			s[index].ErrorChan <- err
+			return
 		}
 
-		body, err := io.ReadAll(res.Body)
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
-			log.Println(err)
+			s[index].ErrorChan <- err
+			return
 		}
-		res.Body.Close()
+		resp.Body.Close()
 		jsonbody := string(body)
-		if s.tokenField == "" {
-			Keyring[index].Token.Store(jsonbody)
+		// If response body token field is specified store it in keyring otherwise store entire response body
+		if s[index].tokenField == "" {
+			s[index].Token.Store(jsonbody)
+			expiry = gjson.Get(jsonbody, "expires_in").Int()
 		} else {
-			Keyring[index].Token.Store(gjson.Get(jsonbody, s.tokenField).Str)
+			s[index].Token.Store(gjson.Get(jsonbody, s[index].tokenField).Str)
 		}
-		expiry = gjson.Get(jsonbody, "expires_in").Int()
 		d := time.Duration(expiry)
 		time.Sleep(d * time.Second)
+		s[index].Token.Store("")
 	}
 }
 
